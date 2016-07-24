33bbb4ff1ce61e62f8890d581d28ff06a062e8e8
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-24 14:55:18 +0530

    Front End Complete: final

--- app/src/main/java/sd_dtu/synergygoadmin/EditActivity.java
+++ app/src/main/java/sd_dtu/synergygoadmin/EditActivity.java
@@ -1,7 +1,10 @@
 package sd_dtu.synergygoadmin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+    }
+    public void onClickEditfinal(View view){
+
+        Toast.makeText(getApplicationContext(), "Edited The Task", Toast.LENGTH_SHORT).show();
+        Intent intent4=new Intent(EditActivity.this,MenuActivity.class);
+        startActivity(intent4);
     }
 }
