7100d7739325e40c50cd5d5c1d03e5d6ecbd18b9
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-24 13:02:45 +0530

    All activities created and only one left to basic edit

--- app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
+++ app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
@@ -1,7 +1,10 @@
 package sd_dtu.synergygoadmin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class NewAllotAct extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class NewAllotAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_allot);
+    }
+
+    public void onClickallocate(View view){
+
+        Toast.makeText(getApplicationContext(), "Allocated New Task", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(NewAllotAct.this,MenuActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/sd_dtu/synergygoadmin/PreviousAllotDetails.java
+++ app/src/main/java/sd_dtu/synergygoadmin/PreviousAllotDetails.java
@@ -1,13 +1,33 @@
 package sd_dtu.synergygoadmin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PreviousAllotDetails extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    String[] agents={"agent1","agent2","agent3","agent4","agent5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_allot_details);
+        ListView agentlv=(ListView)findViewById(R.id.agentlv);
+
+        adapter = new ArrayAdapter<String>(this,R.layout.lv_custom_layout,R.id.list_item,agents);
+        agentlv.setAdapter(adapter);
+
+        agentlv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(PreviousAllotDetails.this,DetailsAct.class);
+                startActivity(intent);
+            }
+        });
     }
 }
