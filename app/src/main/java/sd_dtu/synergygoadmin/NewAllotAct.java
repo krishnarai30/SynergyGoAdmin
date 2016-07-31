5897698691e7d39c58f7ba3c94e0b4e6fc924fb7
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-31 13:59:57 +0530

    Agent + new allocate cmplt

--- app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
+++ app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
@@ -3,11 +3,17 @@ package sd_dtu.synergygoadmin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class NewAllotAct extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class NewAllotAct extends AppCompatActivity {
+    EditText fileet,applicantet,contactpet,contactset,addet,landmarket,agentidet;
+    String file,applicant,contactp,contacts,add,landmark,agentid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,66 @@ public class NewAllotAct extends AppCompatActivity {
     }
 
     public void onClickallocate(View view){
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        agentidet=(EditText)findViewById(R.id.Agentidet);
+        fileet=(EditText)findViewById(R.id.efilenoet);
+        applicantet=(EditText)findViewById(R.id.nameet);
+        contactpet=(EditText)findViewById(R.id.econtactpet);
+        contactset=(EditText)findViewById(R.id.econtactset);
+        addet=(EditText)findViewById(R.id.eaddet);
+        landmarket=(EditText)findViewById(R.id.elandmarket);
+
+        agentid=agentidet.getText().toString().trim();
+        file=fileet.getText().toString().trim();
+        applicant=applicantet.getText().toString();
+        contactp=contactpet.getText().toString().trim();
+        contacts=contactset.getText().toString().trim();
+        add=addet.getText().toString();
+        landmark=landmarket.getText().toString().trim();
+
+        if(TextUtils.isEmpty(agentid)){
+            Toast.makeText(this,"Please enter Agent ID",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(file)){
+            Toast.makeText(this,"Please enter File No.",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(applicant)){
+            Toast.makeText(this,"Please enter Applicant Name",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(contactp)){
+            Toast.makeText(this,"Please enter Primary Contact No.",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(contacts)){
+            Toast.makeText(this,"Please enter Secondary Contact",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(add)){
+            Toast.makeText(this,"Please enter Address",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(landmark)){
+            Toast.makeText(this,"Please enter Landmark",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mDatabase.child(file).child("Agent ID").setValue(agentid);
+        mDatabase.child(file).child("Applicant's name").setValue(applicant);
+        mDatabase.child(file).child("Contact Primary").setValue(contactp);
+        mDatabase.child(file).child("Contact Secondary").setValue(contacts);
+        mDatabase.child(file).child("Address").setValue(add);
+        mDatabase.child(file).child("Landmark").setValue(landmark);
+
+
 
         Toast.makeText(getApplicationContext(), "Allocated New Task", Toast.LENGTH_SHORT).show();
         Intent intent=new Intent(NewAllotAct.this,MenuActivity.class);
--- app/src/main/java/sd_dtu/synergygoadmin/RegisterNewAgent.java
+++ app/src/main/java/sd_dtu/synergygoadmin/RegisterNewAgent.java
@@ -1,21 +1,68 @@
 package sd_dtu.synergygoadmin;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterNewAgent extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    EditText emailet,passet;
+    String email,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_new_agent);
+
     }
     public void onClickRegfinalAgent(View view){
-        Toast.makeText(getApplicationContext(), "Registered New Agent", Toast.LENGTH_SHORT).show();
-        Intent intent=new Intent(RegisterNewAgent.this,MenuActivity.class);
-        startActivity(intent);
+
+        firebaseAuth =FirebaseAuth.getInstance();
+        emailet=(EditText)findViewById(R.id.agentemailidreget);
+        passet=(EditText)findViewById(R.id.agentpassreget);
+
+        email=emailet.getText().toString().trim();
+        pass=passet.getText().toString().trim();
+
+
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Registered New Agent", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(RegisterNewAgent.this,MenuActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Registeration failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
 }
