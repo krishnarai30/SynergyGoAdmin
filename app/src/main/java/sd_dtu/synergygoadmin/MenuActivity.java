7c895f596c08b940097d47a57800d896b245f690
krishnarai30 <krishna.rai30@yahoo.com>
2016-08-18 01:15:41 +0530

    Done fully operational Application bugs removen and awesome working

--- app/src/main/java/sd_dtu/synergygoadmin/MenuActivity.java
+++ app/src/main/java/sd_dtu/synergygoadmin/MenuActivity.java
@@ -1,16 +1,48 @@
 package sd_dtu.synergygoadmin;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MenuActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final String PREFS_NAME = "MyPrefsFile";
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Log.d("Comments", "First time");
+
+            // first time task
+            SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+            editor.putInt("selection-start", 0);
+            editor.apply();
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).apply();
+        }
         setContentView(R.layout.activity_menu);
+
     }
 
     public void onClicknew(View view){
@@ -22,6 +54,7 @@ public class MenuActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void onClickRegAgent(View view){
+
         Intent intent=new Intent(MenuActivity.this,RegisterNewAgent.class);
         startActivity(intent);
     }
--- app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
+++ app/src/main/java/sd_dtu/synergygoadmin/NewAllotAct.java
@@ -1,18 +1,33 @@
 package sd_dtu.synergygoadmin;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class NewAllotAct extends AppCompatActivity {
-    EditText fileet,applicantet,contactpet,contactset,addet,landmarket,agentidet,addtype;
+    EditText fileet,applicantet,contactpet,contactset,addet,landmarket,agentidet;
+    Spinner addtype;
+    int i;
+    String x;
+    ArrayAdapter<CharSequence> addadap;
     String file,applicant,contactp,contacts,add,landmark,agentid,stringaddtype;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +36,7 @@ public class NewAllotAct extends AppCompatActivity {
     }
 
     public void onClickallocate(View view){
-        DatabaseReference mDatabase;
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-
-        addtype=(EditText)findViewById(R.id.addtypeet);
         agentidet=(EditText)findViewById(R.id.Agentidet);
         fileet=(EditText)findViewById(R.id.efilenoet);
         applicantet=(EditText)findViewById(R.id.nameet);
@@ -33,6 +44,32 @@ public class NewAllotAct extends AppCompatActivity {
         contactset=(EditText)findViewById(R.id.econtactset);
         addet=(EditText)findViewById(R.id.eaddet);
         landmarket=(EditText)findViewById(R.id.elandmarket);
+        addtype=(Spinner)findViewById(R.id.addtypspinner);
+
+        addadap=ArrayAdapter.createFromResource(this,R.array.add_type,R.layout.support_simple_spinner_dropdown_item);
+        addadap.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        addtype.setAdapter(addadap);
+        addtype.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i)
+                {
+                    case 0:
+                        stringaddtype="Office";
+                        break;
+                    case 1:
+                        stringaddtype="Residence";
+                        break;
+                    case 2:
+                        stringaddtype=" Business";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                stringaddtype="Residence";
+            }
+        });
 
         agentid=agentidet.getText().toString().trim();
         file=fileet.getText().toString().trim();
@@ -41,7 +78,6 @@ public class NewAllotAct extends AppCompatActivity {
         contacts=contactset.getText().toString().trim();
         add=addet.getText().toString();
         landmark=landmarket.getText().toString().trim();
-        stringaddtype=addtype.getText().toString().trim();
 
         if(TextUtils.isEmpty(agentid)){
             Toast.makeText(this,"Please enter Agent ID",Toast.LENGTH_LONG).show();
@@ -79,15 +115,23 @@ public class NewAllotAct extends AppCompatActivity {
             Toast.makeText(this,"Please enter Address Type",Toast.LENGTH_LONG).show();
             return;
         }
-        mDatabase.child("file").child(file).child("Address Type").setValue(stringaddtype);
-        mDatabase.child("file").child(file).child("Agent ID").setValue(agentid);
-        mDatabase.child("file").child(file).child("Applicant's name").setValue(applicant);
-        mDatabase.child("file").child(file).child("Contact Primary").setValue(contactp);
-        mDatabase.child("file").child(file).child("Contact Secondary").setValue(contacts);
-        mDatabase.child("file").child(file).child("Address").setValue(add);
-        mDatabase.child("file").child(file).child("Landmark").setValue(landmark);
-        mDatabase.child("file").child(file).child("Landmark").setValue(landmark);
-        mDatabase.child("file").child(file).child("File").setValue(file);
+        DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        int i = prefs.getInt("selection-start",-1);
+        i++;
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putInt("selection-start", i);
+        editor.apply();
+        x=Integer.toString(i);
+        mDatabase.child("file").child(x).child("Address Type").setValue(stringaddtype);
+        mDatabase.child("file").child(x).child("Agent ID").setValue(agentid);
+        mDatabase.child("file").child(x).child("Applicant's name").setValue(applicant);
+        mDatabase.child("file").child(x).child("Contact Primary").setValue(contactp);
+        mDatabase.child("file").child(x).child("Contact Secondary").setValue(contacts);
+        mDatabase.child("file").child(x).child("Address").setValue(add);
+        mDatabase.child("file").child(x).child("Landmark").setValue(landmark);
+        mDatabase.child("file").child(x).child("File").setValue(file);
 
 
 
